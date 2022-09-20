bfd7fcc17a70ab114f5009e070ca6994ab82399c
shoji kobayashi <shoji.kobayashi.0125@gmail.com>
2022-09-20 23:02:18 +0900

    map helix keys

--- keyboards/helix/rev2/keymaps/shojik_helix/keymap.c
+++ keyboards/helix/rev2/keymaps/shojik_helix/keymap.c
@@ -49,6 +49,27 @@ enum macro_keycodes {
 //Macros
 #define M_SAMPLE M(KC_SAMPLEMACRO)
 
+//my ja macros
+#define JA_CLON KC_QUOT  // ' -> : and +
+#define JA_AT   KC_LBRC  // [ -> @ and `
+#define JA_HAT  KC_EQL   // = -> ^ and ~
+#define JA_ENUN KC_RO    // not used -> \ and _ (EN mark and UNder score)
+#define JA_ENVL KC_JYEN  // not userd -> \ and | (EN mark and Vertical Line)
+#define JA_LBRC KC_RBRC  // ] -> [ and {
+#define JA_RBRC KC_BSLS  // \ -> ] and }
+
+#define JA_DQUOT KC_AT   // @ -> "
+#define JA_BQUOT KC_LCBR // " -> `
+#define JA_LCBR KC_RCBR  // } -> {
+#define JA_RCBR KC_PIPE  // | -> }
+#define JA_RPRN KC_LPRN  // ( -> )
+#define JA_TRAS KC_RPRN  // ) -> blank
+#define JA_AMPR KC_CIRC  // ^ -> &
+#define JA_TGGL KC_TILD  // ~ -> toggle en/ja
+#define JA_LPRN KC_ASTR  // * -> (
+#define JA_TILD KC_PLUS  // + -> ~
+#define JA_QUOT KC_AMPR  // & -> '
+
 #if MATRIX_ROWS == 10 // HELIX_ROWS == 5
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -66,11 +87,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT(
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-      ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, EISU,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JA_AT,
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, JA_CLON,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PGUP, KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, JA_ENUN,
+      ADJUST,  KC_LGUI, KC_LALT, KC_LEFT, KC_RGHT, KC_SPC,  KC_BSPC,  KC_DEL, KC_ENT,  KC_UP,   KC_DOWN, KC_DOWN, _______, _______
       ),
 
   /* Colemak
@@ -172,10 +193,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_ADJUST] =  LAYOUT(
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-      _______, QK_BOOT,   RGBRST,  _______, _______, _______,                   _______, _______, _______, _______, _______, KC_DEL,
-      _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM,                   AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD
+      _______, _______, _______, JA_LCBR, JA_RCBR, JA_ENVL,                   _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, JA_LPRN, JA_RPRN, JA_TILD,                   KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,
+      _______, _______, _______, JA_LBRC, JA_RBRC, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, KC_HOME,  KC_END, _______,  KC_DEL, _______, _______, KC_PGUP, KC_PGDN, _______, _______, _______
       )
 };
 
