8b6816ecbbc76eee302635473f3fe079aedc106c
shoji kobayashi <shoji.kobayashi.0125@gmail.com>
2022-10-01 17:32:19 +0900

    set ergodox_ez keymaps

--- keyboards/ergodox_ez/keymaps/shojik_ergodox_ez/keymap.c
+++ keyboards/ergodox_ez/keymaps/shojik_ergodox_ez/keymap.c
@@ -15,6 +15,27 @@ enum custom_keycodes {
 #endif
 };
 
+//my ja macros
+#define JA_CLON KC_QUOT  // ' -> : and +
+#define JA_AT   KC_LBRC  // [ -> @ and `
+#define JA_HAT  KC_EQL   // = -> ^ and ~
+#define JA_ENUN KC_RO    // not used -> \ and _ (EN mark and UNder score)
+#define JA_ENVL KC_JYEN  // not userd -> \ and | (EN mark and Vertical Line)
+#define JA_LBRC KC_RBRC  // ] -> [ and {
+#define JA_RBRC KC_BSLS  // \ -> ] and }
+
+#define JA_DQUOT KC_AT   // @ -> "
+#define JA_BQUOT KC_LCBR // " -> `
+#define JA_LCBR KC_RCBR  // } -> {
+#define JA_RCBR KC_PIPE  // | -> }
+#define JA_RPRN KC_LPRN  // ( -> )
+#define JA_TRAS KC_RPRN  // ) -> blank
+#define JA_AMPR KC_CIRC  // ^ -> &
+#define JA_TGGL KC_TILD  // ~ -> toggle en/ja
+#define JA_LPRN KC_ASTR  // * -> (
+#define JA_TILD KC_PLUS  // + -> ~
+#define JA_QUOT KC_AMPR  // & -> '
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
@@ -40,14 +61,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox_pretty(
   // left hand
-  KC_EQL,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,              KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
-  KC_DEL,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB),             TG(SYMB),     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
-  KC_BSPC,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), GUI_T(KC_QUOT),
-  KC_LSFT,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),                  MEH_T(KC_NO), KC_N,    KC_M,    KC_COMM, KC_DOT,           CTL_T(KC_SLSH), KC_RSFT,
-  LT(SYMB,KC_GRV), KC_QUOT,     LALT(KC_LSFT), KC_LEFT, KC_RGHT,                                              KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(SYMB),
-                                                           ALT_T(KC_APP), KC_LGUI,                KC_LALT, CTL_T(KC_ESC),
+  KC_ESC,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_TRNS,              KC_TRNS,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0, KC_MINS,
+  KC_TAB,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB),             TG(SYMB),     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P, JA_AT,
+  KC_LCTL,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,           KC_SCLN, JA_CLON,
+  KC_LSFT,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),     MEH_T(KC_NO),     KC_N,    KC_M,    KC_COMM, KC_DOT,         KC_SLSH, JA_ENUN,
+  LT(SYMB,KC_GRV), KC_LGUI,     KC_LALT, KC_LEFT, KC_RGHT,                                                            KC_UP,   KC_DOWN,  KC_TRNS, KC_TRNS, KC_TRNS,
+                                                           KC_PGUP, KC_LGUI,                KC_LALT, KC_PGDN,
                                                                           KC_HOME,                 KC_PGUP,
-                                                         KC_SPC, KC_BSPC, KC_END,                  KC_PGDN, KC_TAB, KC_ENT
+                                                         KC_SPC, KC_BSPC, KC_END,                  KC_PGDN, KC_DEL, KC_ENT
 ),
 /* Keymap 1: Symbol Layer
  *
@@ -72,14 +93,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [SYMB] = LAYOUT_ergodox_pretty(
   // left hand
-  VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,     KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,     KC_TRNS, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
-  KC_TRNS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,               KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-  KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,     KC_TRNS, KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-  EEP_RST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                         KC_TRNS, KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
-                                               RGB_MOD, KC_TRNS,     RGB_TOG, RGB_M_P,
+  VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,     KC_F12,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+  KC_TRNS, KC_TRNS, KC_TRNS, JA_LCBR, JA_RCBR, JA_ENVL, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_ASTR, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, JA_LPRN, JA_RPRN, JA_TILD,                       KC_LEFT, KC_DOWN,    KC_UP,  KC_RGHT, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, JA_LBRC, JA_RBRC, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
+  EEP_RST, KC_TRNS, KC_TRNS, KC_HOME, KC_END,                                         KC_PGUP,   KC_PGDN,  KC_TRNS, KC_TRNS, KC_TRNS,
+                                               KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,
                                                         KC_TRNS,     KC_TRNS,
-                                      RGB_VAD, RGB_VAI, KC_TRNS,     KC_TRNS, RGB_HUD, RGB_HUI
+                                      KC_TRNS, KC_DEL, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* Keymap 2: Media and mouse keys
  *
